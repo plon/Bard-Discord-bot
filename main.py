52c26ddcd90c598b4e240ffd2f276d22326c89a8
Evan Chiao <evanc314@gmail.com>
2023-07-31 17:02:20 -0400

    API change and config.ini support

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-import os
-from Bard import Chatbot
-import discord
+from bardapi import BardAsync
+import configparser 
+import discord 
 from discord.ext import commands
-import dotenv
 
-# Set up the Discord bot
-dotenv_file = dotenv.find_dotenv()
-dotenv.load_dotenv(dotenv_file)
+config = configparser.ConfigParser()
+config.read('config.ini')
+BARD_TOKEN = config["TOKENS"]['bard_token']
+bard = BardAsync(token=BARD_TOKEN)
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents, heartbeat_timeout=60)
-BARD_TOKEN = os.environ.get('BARD_TOKEN')
-TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
-bard = Chatbot(BARD_TOKEN)
-reply_all = os.environ.get("REPLY_ALL")
-use_images = os.environ.get("USE_IMAGES")
-
-allow_dm = True
-active_channels = set()
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    await bot.change_presence(activity=discord.Game(name="/help"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/help"))
     print(f"{bot.user.name} has connected to Discord!")
     invite_link = discord.utils.oauth_url(
         bot.user.id,
@@ -30,173 +22,105 @@ async def on_ready():
     )
     print(f"Invite link: {invite_link}")
 
-message_id = ""
-images = []
+@bot.tree.command(name="reset", description="Reset chat context")
+async def reset(interaction: discord.Interaction):
+    await interaction.response.defer()
+    global bard
+    bard = BardAsync(token=BARD_TOKEN)
+    await interaction.followup.send("Chat context successfully reset.")
+    return
+    
+
+@bot.tree.command(name="chat", description="Chat with Bard")
+async def chat(interaction: discord.Interaction, prompt: str, image: discord.Attachment = None):
+    await interaction.response.defer()
+    response = await generate_response(prompt) 
+    if len(response['content']) > 2000:
+        embed = discord.Embed(title="Response", description=response['content'], color=0xf1c40f)
+        await interaction.followup.send(embed=embed)
+    else:
+        await interaction.followup.send(response['content'])
+    return
+
 async def generate_response(prompt):
-    global images 
-    max_length = 1900
-    response = bard.ask(prompt)
-    if not response or "Google Bard encountered an error" in response["content"]:
-        response = "I couldn't generate a response. Please try again."
+    max_length = 1950
+    response = await bard.get_answer(prompt)
+    # if "unable to get response" not in response["content"]
+    if not "Unable to get response." in response["content"]:
+        config = read_config()
+        if config.getboolean("SETTINGS", "use_images"):
+            images = response["images"]
+            if images:
+                for image in images:
+                    response["content"] += f"\n{image}"
+        # check image perms
+        # check tts terms
         return response
-    images = response["images"]
-    words = response["content"].split()
-    chunks = []
-    current_chunk = []
+    
+@bot.tree.command(name="public", description = "Bot will respond to all messages")
+async def public(interaction: discord.Interaction):
+    config = read_config()
+    if config.getboolean("SETTINGS", "reply_all"):
+        await interaction.response.send_message("Bot is already in public mode")
+    else:
+        config["SETTINGS"]["reply_all"] = "True"
+        await interaction.response.send_message("Bot will now respond to all messages")
+    write_config(config)
+    return
 
-    for word in words:
-        if len(" ".join(current_chunk)) + len(word) + 1 > max_length:
-            chunks.append(" ".join(current_chunk))
-            current_chunk = [word]
-        else:
-            current_chunk.append(word)
-
-    if current_chunk:
-        chunks.append(" ".join(current_chunk))
-
-    formatted_chunks = []
-    for chunk in chunks:
-        formatted_chunk = chunk.replace(" * ", "\n* ")
-        formatted_chunks.append(formatted_chunk)
-        
-    return formatted_chunks
+@bot.tree.command(name="private", description = "Bot will only respond to /chat")
+async def private(interaction: discord.Interaction):
+    config = read_config()
+    if not config.getboolean("SETTINGS", "reply_all"):
+        config["SETTINGS"]["reply_all"] = "false"
+        await interaction.response.send_message("Bot will now only respond to /chat")
+    else:
+        await interaction.response.send_message("Bot is already in private mode")
+    write_config(config)
+    return
 
 @bot.event
 async def on_message(message):
-    global images
-    reply_all = os.environ.get('REPLY_ALL', '').lower() == 'true'
-    if reply_all:
-        if message.author.bot:
+    config = read_config()
+    if config.getboolean("SETTINGS", "reply_all"):
+        if message.author == bot.user:
             return
-        if message.reference and message.reference.resolved.author != bot.user:
-            return  # Ignore replies to messages not from the bot
-        is_dm_channel = isinstance(message.channel, discord.DMChannel)
-    
-        if message.channel.id in active_channels or (allow_dm and is_dm_channel):
-            user_prompt = message.content
-            async with message.channel.typing():
-                response = await generate_response(user_prompt)
-            for chunk in response:
-                await message.reply(chunk)
-            if use_images.lower() == 'true':
-                if images:
-                    for image in images:
-                        await message.reply(image)
+        async with message.channel.typing():
+            response = await generate_response(message.content)
+            if len(response['content']) > 2000:
+                embed = discord.Embed(title="Response", description=response['content'], color=0xf1c40f)
+                await message.channel.send(embed=embed)
+            else:
+                await message.channel.send(response['content'])
 
-@bot.hybrid_command(name="toggledm", description="Toggle DM for chatting.")
-async def toggledm(ctx):
-    global allow_dm
-    allow_dm = not allow_dm
-    await ctx.send(f"DMs are now {'allowed' if allow_dm else 'disallowed'} for active channels.")
-    
-@bot.hybrid_command(name="togglechannel", description="Toggle active channels.")
-async def toggleactive(ctx):
-    channel_id = ctx.channel.id
-    if channel_id in active_channels:
-        active_channels.remove(channel_id)
-        with open("channels.txt", "w") as f:
-            for id in active_channels:
-                f.write(str(id) + "\n")
-        await ctx.send(
-            f"{ctx.channel.mention} has been removed from the list of active channels."
-        )
+@bot.tree.command(name="images", description="Toggle if bot should respond with images")
+async def images(interaction: discord.Interaction):
+    config = read_config()
+    if config.getboolean("SETTINGS", "use_images"):
+        config["SETTINGS"]["use_images"] = "false"
+        await interaction.response.send_message("Bot will no longer respond with images")
     else:
-        active_channels.add(channel_id)
-        with open("channels.txt", "a") as f:
-            f.write(str(channel_id) + "\n")
-        await ctx.send(
-            f"{ctx.channel.mention} has been added to the list of active channels!")
-        
-@bot.hybrid_command(name="reset", description="Reset the bot's context.")
-async def reset(ctx):
-    bard.conversation_id = ""
-    bard.response_id = ""
-    bard.choice_id = ""
-    await ctx.send("Bot context has been reset.")
+        config["SETTINGS"]["use_images"] = "true"
+        await interaction.response.send_message("Bot will now respond with images")
+    write_config(config)
+    return
 
-# Read the active channels from channels.txt on startup
-if os.path.exists("channels.txt"):
-    with open("channels.txt", "r") as f:
-        for line in f:
-            channel_id = int(line.strip())
-            active_channels.add(channel_id)
+@bot.tree.command(name="help", description="Get all commands")
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(title="Commands", description="All commands for the bot", color=0xf1c40f)
+    embed.add_field(name="/chat", value="Chat with Bard", inline=False)
+    embed.add_field(name="/reset", value="Reset chat context", inline=False)
+    embed.add_field(name="/public", value="Set bot to respond to all messages", inline=False)
+    embed.add_field(name="/private", value="Set bot to only respond to /chat", inline=False)
+    embed.add_field(name="/images", value="Toggle if bot should respond with images", inline=False)
 
-@bot.hybrid_command(name="public", description="Toggle if bot should only respond to /chat or all messages in chat.")
-async def public(ctx):
-    global reply_all
-    if os.environ.get('REPLY_ALL', '').lower() == 'false':
-        reply_all = True 
-        dotenv.set_key(dotenv_file, "REPLY_ALL", str(reply_all))
-        os.environ['REPLY_ALL'] = str(reply_all)
-        await ctx.send(f"Bot will now respond to all messages in chat.")
-        return
-    else:
-        await ctx.send(f"Bot is already in public mode.")
-        return
+def read_config():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
 
-@bot.hybrid_command(name="private", description="Toggle if bot should only respond to /chat or all messages in chat.")
-async def private(ctx):
-    global reply_all
-    if os.environ.get('REPLY_ALL', '').lower() == 'true':
-        reply_all = False 
-        dotenv.set_key(dotenv_file, "REPLY_ALL", str(reply_all))
-        os.environ['REPLY_ALL'] = str(reply_all)
-        await ctx.send(f"Bot will now only respond to /chat.")
-        return
-    else:
-        await ctx.send(f"Bot is already in private mode.")
-        return
+def write_config(config):
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
 
-@bot.hybrid_command(name="images", description="Toggle if bot should respond with images")
-async def images(ctx):
-    global use_images
-    if os.environ.get('USE_IMAGES', '').lower() == 'false':
-        use_images = 'true'
-        dotenv.set_key(dotenv_file, "USE_IMAGES", str(use_images))
-        os.environ['USE_IMAGES'] = str(use_images)
-        await ctx.send(f"Bot will now respond with images.")
-        return
-    if os.environ.get('USE_IMAGES', '').lower() == 'true':
-        use_images = 'false'
-        dotenv.set_key(dotenv_file, "USE_IMAGES", str(use_images))
-        os.environ['USE_IMAGES'] = str(use_images)
-        await ctx.send(f"Bot will now respond with text.")
-        return
-
-@bot.tree.command(name="chat", description="Have a chat with Bard")
-async def chat(interaction: discord.Interaction, message: str):
-    global images
-    await interaction.response.defer()
-    is_dm_channel = isinstance(interaction.channel, discord.DMChannel)
-    if interaction.user == bot.user:
-        return
-    if interaction.channel.id in active_channels or (allow_dm & is_dm_channel):
-        allowed_mentions = discord.AllowedMentions(users=False)
-        interaction_response = (f'> **{message}** - {interaction.user.mention} \n\n')      
-        response = await generate_response(message)
-        await interaction.followup.send(interaction_response, allowed_mentions=allowed_mentions)
-        for chunk in response:
-            try:
-                await interaction.channel.send(chunk)
-            except discord.errors.HTTPException:
-                await interaction.channel.send("I couldn't generate a response. Please try again.")
-        if use_images.lower() == 'true':
-            if images:
-                for image in images:
-                    await interaction.channel.send(image)
-
-bot.remove_command("help")   
-@bot.hybrid_command(name="help", description="Get all other commands!")
-async def help(ctx):
-    embed = discord.Embed(title="Bot Commands", color=0x00ff00)
-    embed.add_field(name="/chat", value="Have a chat with Bard.", inline=False)
-    embed.add_field(name="/reset", value="Reset bot's context.", inline=False)
-    embed.add_field(name="/togglechannel", value="Add the channel you are currently in to the Active Channel List.", inline=False)   
-    embed.add_field(name="/toggledm", value="Toggle if DM chatting should be active", inline=False)
-    embed.add_field(name="/public", value ="Toggle if bot should respond to all messages in chat", inline=False)
-    embed.add_field(name="/private", value ="Toggle if bot should only respond to /chat", inline=False)
-    embed.add_field(name="/images", value ="Toggle if bot should respond with images", inline=False)
-    
-    await ctx.send(embed=embed)
-
-bot.run(TOKEN)
+bot.run(config["TOKENS"]['discord_bot_token'])
